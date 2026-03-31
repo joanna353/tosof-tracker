997a2f3ab873b0021af44b9459fbb51763c9c3f2
joanna353 <joanna@theothersideoffear.co.uk>
2026-03-31 22:15:56 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 export default function Home() {
   return (
-    <div style={{margin: 0, padding: 0}}>
+    <div style={{margin:0,padding:0,height:'100vh'}}>
       <iframe 
         src="/tracker.html" 
-        style={{
-          width: '100%',
-          height: '100vh',
-          border: 'none',
-          display: 'block'
-        }}
+        style={{width:'100%',height:'100vh',border:'none',display:'block'}}
+        allow="same-origin"
       />
     </div>
   )
