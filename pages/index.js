dbba57653e8f92807704d23cec99c437ca70d52a
joanna353 <joanna@theothersideoffear.co.uk>
2026-03-31 16:23:18 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+jsexport default function Home() {
+  return (
+    <div style={{margin: 0, padding: 0}}>
+      <iframe 
+        src="/tracker.html" 
+        style={{
+          width: '100%',
+          height: '100vh',
+          border: 'none',
+          display: 'block'
+        }}
+      />
+    </div>
+  )
+}
